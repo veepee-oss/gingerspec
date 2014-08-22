fda0684887e49476fd0676f8eef70e5a5ab7a5db
witokondoria <witokondoria@gmail.com>
2014-08-22 11:56:35 +0200

    String parameters can be passed now as hex chars

--- src/main/java/com/stratio/cucumber/converter/StreamNameConverter.java
+++ src/main/java/com/stratio/cucumber/converter/StreamNameConverter.java
@@ -9,6 +9,11 @@ public class StreamNameConverter extends Transformer<String> {
 
 		if (input.equals("//NULL//")) {
 			return null;
+		} else if (input.startsWith("0x")) {
+
+			int cInt = Integer.parseInt(input.substring(2), 16);
+			char[] cArr = Character.toChars(cInt);			
+			return String.valueOf(cArr);
 		} else {
 			return input;
 		}
