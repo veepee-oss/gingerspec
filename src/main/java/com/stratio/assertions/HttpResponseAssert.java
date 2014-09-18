2315fcf02f1c74c5737ec71943679b4943ac867c
witokondoria <witokondoria@gmail.com>
2014-09-18 17:14:43 +0200

    Added new negated asserts

--- src/main/java/com/stratio/assertions/HttpResponseAssert.java
+++ src/main/java/com/stratio/assertions/HttpResponseAssert.java
@@ -24,11 +24,30 @@ public class HttpResponseAssert extends
         return this;
     }
 
+    public HttpResponseAssert doesNotHaveStatusCode(Integer status) {
+        if (actual.getStatusCode() == status) {
+            failWithMessage(
+                    "Expected response status code not to be <%s> but was <%s>",
+                    status, actual.getStatusCode());
+        }
+        return this;
+    }
+
     public HttpResponseAssert hasMessage(String message) {
 
         if (!actual.getResponse().contains(message)) {
             failWithMessage(
                     "Expected response message to contain <%s> but was <%s>",
+                    message, actual.getResponse());
+        }
+        return this;
+    }
+
+    public HttpResponseAssert doesNotHaveMessage(String message) {
+
+        if (actual.getResponse().contains(message)) {
+            failWithMessage(
+                    "Expected response message not to contain <%s> but was <%s>",
                     message, actual.getResponse());
         }
         return this;
@@ -55,6 +74,31 @@ public class HttpResponseAssert extends
         if (!"".equals(msg)) {
             failWithMessage(msg);
         }
+        return this;
+    }
+
+    
+    public HttpResponseAssert doesNotHaveStatusCodeNorMessage(Integer status,
+            String message) {
+
+         String msg = "";
+         if (actual.getStatusCode() == status) {
+         msg += String.format(
+         "Expected response status code not to be <%s> but was <%s>",
+         status, actual.getStatusCode());
+         }
+         if (actual.getResponse().contains(message)) {
+         String nl = "";
+         if (!"".equals(msg)) {
+         nl = String.format("%s%26s", System.lineSeparator(), " ");
+         }
+         msg += String.format(
+         "%sExpected response message not to contain <%s> but was <%s>",
+         nl, message, actual.getResponse());
+         }
+         if (!"".equals(msg)) {
+         failWithMessage(msg);
+         }
         return this;
     }
 
