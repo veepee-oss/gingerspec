6a3c9fdd65da71ac065af9f1e8c4ce92eafd0e89
Hugo Dominguez <hdominguez@stratio.com>
2015-08-05 12:34:08 +0200

    Fix error in CassandraUtils(LoadScript)

--- lib/src/main/java/com/stratio/tests/utils/CassandraUtils.java
+++ lib/src/main/java/com/stratio/tests/utils/CassandraUtils.java
@@ -27,300 +27,314 @@ import com.stratio.exceptions.DBException;
  */
 public class CassandraUtils {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CassandraUtils.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(CassandraUtils.class);
 
-    private Cluster cluster;
-    private final String host;
-    private Metadata metadata;
-    private Session session;
-    private CassandraQueryUtils CassandraqueryUtils;
+	private Cluster cluster;
+	private final String host;
+	private Metadata metadata;
+	private Session session;
+	private CassandraQueryUtils CassandraqueryUtils;
 
-    /**
-     * Generic contructor of CassandraUtils.
-     */
-    public CassandraUtils() {
-        this.host = System.getProperty("CASSANDRA_HOST", "127.0.0.1");
-    }
+	/**
+	 * Generic contructor of CassandraUtils.
+	 */
+	public CassandraUtils() {
+		this.host = System.getProperty("CASSANDRA_HOST", "127.0.0.1");
+	}
 
-    /**
-     * Connect to Cassandra host.
-     */
-    public void connect() {
-        buildCluster();
-        this.CassandraqueryUtils = new CassandraQueryUtils();
-        this.metadata = this.cluster.getMetadata();
-        LOGGER.debug("Connected to cluster (" + host + "): " + metadata.getClusterName() + "\n");
-        this.session = this.cluster.connect();
-    }
+	/**
+	 * Connect to Cassandra host.
+	 */
+	public void connect() {
+		buildCluster();
+		this.CassandraqueryUtils = new CassandraQueryUtils();
+		this.metadata = this.cluster.getMetadata();
+		LOGGER.debug("Connected to cluster (" + host + "): "
+				+ metadata.getClusterName() + "\n");
+		this.session = this.cluster.connect();
+	}
 
-    /**
-     * Execute a query over Cassandra.
-     * 
-     * @param query
-     * @return ResultSet
-     */
-    public ResultSet executeQuery(String query) {
+	/**
+	 * Execute a query over Cassandra.
+	 * 
+	 * @param query
+	 * @return ResultSet
+	 */
+	public ResultSet executeQuery(String query) {
 
-        return this.session.execute(query);
-    }
+		return this.session.execute(query);
+	}
 
-    /**
-     * Execute a list of queries over Cassandra.
-     * 
-     * @param queriesList
-     */
-    public void executeQueriesList(List<String> queriesList) {
+	/**
+	 * Execute a list of queries over Cassandra.
+	 * 
+	 * @param queriesList
+	 */
+	public void executeQueriesList(List<String> queriesList) {
 
-        for (String query : queriesList) {
-            this.session.execute(query);
-        }
-    }
+		for (String query : queriesList) {
+			this.session.execute(query);
+		}
+	}
 
-    /**
-     * Reconnect to Cassandra host.
-     */
-    public void reconnect() {
-        metadata = cluster.getMetadata();
-        LOGGER.debug("Connected to cluster (" + host + "): " + metadata.getClusterName() + "\n");
-        this.session = this.cluster.connect();
-    }
+	/**
+	 * Reconnect to Cassandra host.
+	 */
+	public void reconnect() {
+		metadata = cluster.getMetadata();
+		LOGGER.debug("Connected to cluster (" + host + "): "
+				+ metadata.getClusterName() + "\n");
+		this.session = this.cluster.connect();
+	}
 
-    /**
-     * Disconnect from Cassandra host.
-     * @throws DBException 
-     */
-    public void disconnect() throws DBException {
-        if (this.session == null) {
-            throw new DBException ("The C* is null");
-        } 
-        if(this.cluster.isClosed()){
-            throw new DBException ("The cluster has been closed");
-        }
-            this.session.close();
-            this.cluster.close();
-    }
+	/**
+	 * Disconnect from Cassandra host.
+	 * 
+	 * @throws DBException
+	 */
+	public void disconnect() throws DBException {
+		if (this.session == null) {
+			throw new DBException("The C* is null");
+		}
+		if (this.cluster.isClosed()) {
+			throw new DBException("The cluster has been closed");
+		}
+		this.session.close();
+		this.cluster.close();
+	}
 
-    /**
-     * Get the metadata of the Cassandra Cluster.
-     * 
-     * @return Metadata
-     * @throws Exception 
-     */
-    public Metadata getMetadata() throws DBException {
-        if (!this.cluster.isClosed()) {
-            this.metadata = cluster.getMetadata();
-            return this.metadata;
-        } else {
-           throw new DBException ("The cluster has been closed");
-        }
-    }
+	/**
+	 * Get the metadata of the Cassandra Cluster.
+	 * 
+	 * @return Metadata
+	 * @throws Exception
+	 */
+	public Metadata getMetadata() throws DBException {
+		if (!this.cluster.isClosed()) {
+			this.metadata = cluster.getMetadata();
+			return this.metadata;
+		} else {
+			throw new DBException("The cluster has been closed");
+		}
+	}
 
-    /**
-     * Build a Cassandra cluster.
-     */
-    public void buildCluster() {
-        this.cluster = Cluster.builder().addContactPoint(this.host).build();
-        this.cluster.getConfiguration().getQueryOptions().setConsistencyLevel(ConsistencyLevel.ONE);
+	/**
+	 * Build a Cassandra cluster.
+	 */
+	public void buildCluster() {
+		this.cluster = Cluster.builder().addContactPoint(this.host).build();
+		this.cluster.getConfiguration().getQueryOptions()
+				.setConsistencyLevel(ConsistencyLevel.ONE);
 
-    }
+	}
 
-    /**
-     * Get the cassandra session.
-     * 
-     * @return Session
-     */
-    public Session getSession() {
-        return this.session;
-    }
+	/**
+	 * Get the cassandra session.
+	 * 
+	 * @return Session
+	 */
+	public Session getSession() {
+		return this.session;
+	}
 
-    /**
-     * Create a keyspace in Cassandra.
-     * 
-     * @param keyspace
-     */
-    public void createKeyspace(String keyspace) {
-        Map<String, String> replicationSimpleOneExtra = new HashMap<>();
-        replicationSimpleOneExtra.put("'class'", "'SimpleStrategy'");
-        replicationSimpleOneExtra.put("'replication_factor'", "1");
-        String query = this.CassandraqueryUtils.createKeyspaceQuery(true, keyspace,
-                this.CassandraqueryUtils.createKeyspaceReplication(replicationSimpleOneExtra), "");
-        LOGGER.debug(query);
-        executeQuery(query);
-    }
+	/**
+	 * Create a keyspace in Cassandra.
+	 * 
+	 * @param keyspace
+	 */
+	public void createKeyspace(String keyspace) {
+		Map<String, String> replicationSimpleOneExtra = new HashMap<>();
+		replicationSimpleOneExtra.put("'class'", "'SimpleStrategy'");
+		replicationSimpleOneExtra.put("'replication_factor'", "1");
+		String query = this.CassandraqueryUtils.createKeyspaceQuery(true,
+				keyspace, this.CassandraqueryUtils
+						.createKeyspaceReplication(replicationSimpleOneExtra),
+				"");
+		LOGGER.debug(query);
+		executeQuery(query);
+	}
 
-    /**
-     * Checks if a keyspace exists in Cassandra.
-     * 
-     * @param keyspace
-     * @param showLog
-     * @return boolean
-     */
-    public boolean existsKeyspace(String keyspace, boolean showLog) {
-        this.metadata = cluster.getMetadata();
-        if (this.metadata.getKeyspaces().isEmpty()) {
-            return false;
-        }
-        for (KeyspaceMetadata k : metadata.getKeyspaces()) {
-            if (showLog) {
-                LOGGER.debug(k.getName());
-            }
-            if (k.getName().equals(keyspace)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Checks if a keyspace exists in Cassandra.
+	 * 
+	 * @param keyspace
+	 * @param showLog
+	 * @return boolean
+	 */
+	public boolean existsKeyspace(String keyspace, boolean showLog) {
+		this.metadata = cluster.getMetadata();
+		if (this.metadata.getKeyspaces().isEmpty()) {
+			return false;
+		}
+		for (KeyspaceMetadata k : metadata.getKeyspaces()) {
+			if (showLog) {
+				LOGGER.debug(k.getName());
+			}
+			if (k.getName().equals(keyspace)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Get a list of the existing keyspaces in Cassandra.
-     * 
-     * @return List<String>
-     */
-    public List<String> getKeyspaces() {
-        ArrayList<String> result = new ArrayList<String>();
-        this.metadata = this.cluster.getMetadata();
-        if (metadata.getKeyspaces().isEmpty()) {
-            return result;
-        }
-        for (KeyspaceMetadata k : this.metadata.getKeyspaces()) {
-            result.add(k.getName());
-        }
-        return result;
-    }
+	/**
+	 * Get a list of the existing keyspaces in Cassandra.
+	 * 
+	 * @return List<String>
+	 */
+	public List<String> getKeyspaces() {
+		ArrayList<String> result = new ArrayList<String>();
+		this.metadata = this.cluster.getMetadata();
+		if (metadata.getKeyspaces().isEmpty()) {
+			return result;
+		}
+		for (KeyspaceMetadata k : this.metadata.getKeyspaces()) {
+			result.add(k.getName());
+		}
+		return result;
+	}
 
-    /**
-     * Drop a keyspace in Cassandra.
-     * 
-     * @param keyspace
-     */
-    public void dropKeyspace(String keyspace) {
-        executeQuery(this.CassandraqueryUtils.dropKeyspaceQuery(false, keyspace));
-    }
+	/**
+	 * Drop a keyspace in Cassandra.
+	 * 
+	 * @param keyspace
+	 */
+	public void dropKeyspace(String keyspace) {
+		executeQuery(this.CassandraqueryUtils
+				.dropKeyspaceQuery(false, keyspace));
+	}
 
-    /**
-     * Drop a keyspace in Cassandra.
-     * 
-     * @param ifExists
-     * @param keyspace
-     */
-    public void dropKeyspace(boolean ifExists, String keyspace) {
-        executeQuery(this.CassandraqueryUtils.dropKeyspaceQuery(ifExists, keyspace));
-    }
+	/**
+	 * Drop a keyspace in Cassandra.
+	 * 
+	 * @param ifExists
+	 * @param keyspace
+	 */
+	public void dropKeyspace(boolean ifExists, String keyspace) {
+		executeQuery(this.CassandraqueryUtils.dropKeyspaceQuery(ifExists,
+				keyspace));
+	}
 
-    /**
-     * Use a keyspace in Cassandra.
-     * 
-     * @param keyspace
-     */
-    public void useKeyspace(String keyspace) {
-        executeQuery(this.CassandraqueryUtils.useQuery(keyspace));
-    }
+	/**
+	 * Use a keyspace in Cassandra.
+	 * 
+	 * @param keyspace
+	 */
+	public void useKeyspace(String keyspace) {
+		executeQuery(this.CassandraqueryUtils.useQuery(keyspace));
+	}
 
-    /**
-     * Checks if a keyspace contains an especific table.
-     * 
-     * @param keyspace
-     * @param table
-     * @param showLog
-     * @return boolean
-     */
-    public boolean existsTable(String keyspace, String table, boolean showLog) {
-        this.metadata = this.cluster.getMetadata();
+	/**
+	 * Checks if a keyspace contains an especific table.
+	 * 
+	 * @param keyspace
+	 * @param table
+	 * @param showLog
+	 * @return boolean
+	 */
+	public boolean existsTable(String keyspace, String table, boolean showLog) {
+		this.metadata = this.cluster.getMetadata();
 
-        if (this.metadata.getKeyspace(keyspace).getTables().isEmpty()) {
-            return false;
-        }
-        for (TableMetadata t : this.metadata.getKeyspace(keyspace).getTables()) {
-            if (showLog && (t.getName() != null)) {
-                LOGGER.debug(t.getName());
-            }
-            if (t.getName().equals(table)) {
-                return true;
-            }
-        }
-        return false;
-    }
+		if (this.metadata.getKeyspace(keyspace).getTables().isEmpty()) {
+			return false;
+		}
+		for (TableMetadata t : this.metadata.getKeyspace(keyspace).getTables()) {
+			if (showLog && (t.getName() != null)) {
+				LOGGER.debug(t.getName());
+			}
+			if (t.getName().equals(table)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Get tables of a keyspace.
-     * 
-     * @param keyspace
-     * @return List<String>
-     */
-    public List<String> getTables(String keyspace) {
-        ArrayList<String> result = new ArrayList<String>();
-        this.metadata = this.cluster.getMetadata();
-        if (!existsKeyspace(keyspace, false)) {
-            return result;
-        }
-        if (this.metadata.getKeyspace(keyspace).getTables().isEmpty()) {
-            return result;
-        }
-        for (TableMetadata t : this.metadata.getKeyspace(keyspace).getTables()) {
-            result.add(t.getName());
-        }
-        return result;
-    }
+	/**
+	 * Get tables of a keyspace.
+	 * 
+	 * @param keyspace
+	 * @return List<String>
+	 */
+	public List<String> getTables(String keyspace) {
+		ArrayList<String> result = new ArrayList<String>();
+		this.metadata = this.cluster.getMetadata();
+		if (!existsKeyspace(keyspace, false)) {
+			return result;
+		}
+		if (this.metadata.getKeyspace(keyspace).getTables().isEmpty()) {
+			return result;
+		}
+		for (TableMetadata t : this.metadata.getKeyspace(keyspace).getTables()) {
+			result.add(t.getName());
+		}
+		return result;
+	}
 
-    /**
-     * Drop a table of a keyspace.
-     * 
-     * @param keyspace
-     * @param table
-     */
-    public void dropTable(String keyspace, String table) {
-        // se elimina la table
-        executeQuery(this.CassandraqueryUtils.dropTableQuery(false, table));
-    }
+	/**
+	 * Drop a table of a keyspace.
+	 * 
+	 * @param keyspace
+	 * @param table
+	 */
+	public void dropTable(String keyspace, String table) {
+		// se elimina la table
+		executeQuery(this.CassandraqueryUtils.dropTableQuery(false, table));
+	}
 
-    /**
-     * Load a {@code keyspace} in Cassandra using the CQL sentences in the script path. The script is executed if the
-     * keyspace does not exists in Cassandra.
-     * 
-     * @param keyspace
-     *            The name of the keyspace.
-     * @param path
-     *            The path of the CQL script.
-     */
-    public void loadTestData(String keyspace, String path) {
-        KeyspaceMetadata md = session.getCluster().getMetadata().getKeyspace(keyspace);
-        if (md == null) {
-            LOGGER.info("Creating keyspace " + keyspace + " using " + path);
-            List<String> scriptLines = loadScript(path);
-            LOGGER.info("Executing " + scriptLines.size() + " lines");
-            for (String cql : scriptLines) {
-                ResultSet result = session.execute(cql);
-                if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("Executing: " + cql + " -> " + result.toString());
-                }
-            }
-        }
-        LOGGER.info("Using existing keyspace " + keyspace);
-    }
+	/**
+	 * Load a {@code keyspace} in Cassandra using the CQL sentences in the
+	 * script path. The script is executed if the keyspace does not exists in
+	 * Cassandra.
+	 * 
+	 * @param keyspace
+	 *            The name of the keyspace.
+	 * @param path
+	 *            The path of the CQL script.
+	 */
+	public void loadTestData(String keyspace, String path) {
+		KeyspaceMetadata md = session.getCluster().getMetadata()
+				.getKeyspace(keyspace);
+		if (md == null) {
+			LOGGER.info("Creating keyspace " + keyspace + " using " + path);
+			createKeyspace(keyspace);
+		}
+		List<String> scriptLines = loadScript(path);
+		LOGGER.info("Executing " + scriptLines.size() + " lines");
+		for (String cql : scriptLines) {
+			ResultSet result = session.execute(cql);
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Executing: " + cql + " -> " + result.toString());
+			}
+		}
+		LOGGER.info("Using existing keyspace " + keyspace);
+	}
 
-    /**
-     * Load the lines of a CQL script containing one statement per line into a list. l
-     * 
-     * @param path
-     *            The path of the CQL script.
-     * @return The contents of the script.
-     */
-    public static List<String> loadScript(String path) {
-        List<String> result = new ArrayList<String>();
-        URL url = CassandraUtils.class.getResource(path);
-        LOGGER.debug(url.toString());
-        LOGGER.info("Loading script from: " + url);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF8"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.length() > 0 && !line.startsWith("#")) {
-                    result.add(line);
-                }
-            }
-        } catch (IOException e) {
-            LOGGER.error("IO Exception loading a cql script", e);
-        }
-        return result;
-    }
+	/**
+	 * Load the lines of a CQL script containing one statement per line into a
+	 * list. l
+	 * 
+	 * @param path
+	 *            The path of the CQL script.
+	 * @return The contents of the script.
+	 */
+	public static List<String> loadScript(String path) {
+		List<String> result = new ArrayList<String>();
+		URL url = CassandraUtils.class.getResource(path);
+		LOGGER.debug(url.toString());
+		LOGGER.info("Loading script from: " + url);
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(
+				url.openStream(), "UTF8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.length() > 0 && !line.startsWith("#")) {
+					result.add(line);
+				}
+			}
+		} catch (IOException e) {
+			LOGGER.error("IO Exception loading a cql script", e);
+		}
+		return result;
+	}
 
 }
