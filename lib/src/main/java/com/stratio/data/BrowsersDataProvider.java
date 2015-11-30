6834434a5745088eac571a449d420baffefaa885
antonioalf <aalfonso@stratio.com>
2015-11-30 12:44:54 +0100

    QA-26 remove BrowsersDataProvider error messages

--- lib/src/main/java/com/stratio/data/BrowsersDataProvider.java
+++ lib/src/main/java/com/stratio/data/BrowsersDataProvider.java
@@ -17,6 +17,7 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
 import org.testng.annotations.DataProvider;
 
 import com.google.common.collect.Lists;
@@ -59,7 +60,7 @@ public final class BrowsersDataProvider {
     @DataProvider(parallel = true)
     public static Iterator<String[]> availableUniqueBrowsers(ITestContext context, Constructor<?> testConstructor)
             throws Exception {
-
+	
         List<String> browsers = gridBrowsers();
 
         HashSet<String> hs = new HashSet<String>();
@@ -82,51 +83,54 @@ public final class BrowsersDataProvider {
     }
 
     private static List<String> gridBrowsers() {
-        ArrayList<String> response = new ArrayList<String>();
+	ArrayList<String> response = new ArrayList<String>();
 
-        String grid = System.getProperty("SELENIUM_GRID", "127.0.0.1:4444");
-        grid = "http://" + grid + "/grid/console";
-        Document doc;
-        try {
-            doc = Jsoup.connect(grid).timeout(DEFAULT_TIMEOUT).get();
-        } catch (IOException e) {
-            LOGGER.error("Exception on connecting to Selenium grid: {}", e.getMessage());
-            return response;
-        }
+	String grid = System.getProperty("SELENIUM_GRID");
 
-        Elements slaves = (Elements) doc.select("div.proxy");
+	if (grid != null) {        
+	    grid = "http://" + grid + "/grid/console";
+	    Document doc;
+	    try {
+		doc = Jsoup.connect(grid).timeout(DEFAULT_TIMEOUT).get();
+	    } catch (IOException e) {
+		LOGGER.debug("Exception on connecting to Selenium grid: {}", e.getMessage());
+		return response;
+	    }
 
-        for (Element slave : slaves) {
-            String slaveStatus = slave.select("p.proxyname").first().text();
-            if (!slaveStatus.contains("Connection") && !slaveStatus.contains("Conexión")) {
-                Integer iBusy = 0;
-                Elements browserList = slave.select("div.content_detail").select("*[title]");
-                Elements busyBrowserList = slave.select("div.content_detail").select("p > .busy");
-                for (Element browserDetails : browserList) {
-                    if (browserDetails.attr("title").startsWith("{")) {
-                        Pattern pat = Pattern.compile("browserName=(.*?),.*?(version=(.*?))?}");
-                        Matcher m = pat.matcher(browserDetails.attr("title"));
-                        while (m.find()) {
-                            response.add(m.group(1) + "_" + m.group(3));
-                        }
-                    } else {
-                        String version = busyBrowserList.get(iBusy).parent().text();
-                        String browser = busyBrowserList.get(iBusy).text();
-                        version = version.substring(2);
-                        version = version.replace(browser, "");
-                        String browserSrc = busyBrowserList.get(iBusy).select("img").attr("src");
-                        if (!browserSrc.equals("")) {
-                            browser = browserSrc.substring(browserSrc.lastIndexOf('/') + 1, browserSrc.length()
-                                    - DEFAULT_LESS_LENGTH);
-                        }
-                        response.add(browser + "_" + version);
-                        iBusy++;
-                    }
-                }
-            }
-        }
-        // Sort response
-        Collections.sort(response);
-        return response;
+	    Elements slaves = (Elements) doc.select("div.proxy");
+
+	    for (Element slave : slaves) {
+		String slaveStatus = slave.select("p.proxyname").first().text();
+		if (!slaveStatus.contains("Connection") && !slaveStatus.contains("Conexión")) {
+		    Integer iBusy = 0;
+		    Elements browserList = slave.select("div.content_detail").select("*[title]");
+		    Elements busyBrowserList = slave.select("div.content_detail").select("p > .busy");
+		    for (Element browserDetails : browserList) {
+			if (browserDetails.attr("title").startsWith("{")) {
+			    Pattern pat = Pattern.compile("browserName=(.*?),.*?(version=(.*?))?}");
+			    Matcher m = pat.matcher(browserDetails.attr("title"));
+			    while (m.find()) {
+				response.add(m.group(1) + "_" + m.group(3));
+			    }
+			} else {
+			    String version = busyBrowserList.get(iBusy).parent().text();
+			    String browser = busyBrowserList.get(iBusy).text();
+			    version = version.substring(2);
+			    version = version.replace(browser, "");
+			    String browserSrc = busyBrowserList.get(iBusy).select("img").attr("src");
+			    if (!browserSrc.equals("")) {
+				browser = browserSrc.substring(browserSrc.lastIndexOf('/') + 1, browserSrc.length()
+					- DEFAULT_LESS_LENGTH);
+			    }
+			    response.add(browser + "_" + version);
+			    iBusy++;
+			}
+		    }
+		}
+	    }
+	}
+	// Sort response
+	Collections.sort(response);
+	return response;
     }
 }
