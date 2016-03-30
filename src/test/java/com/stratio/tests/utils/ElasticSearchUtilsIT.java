a3247fa490df12dc9fb6101fb236e7eaf2159737
alejandro2003 <alejandro2003@ukr.net>
2016-03-30 09:44:26 +0200

    setHost addition

--- src/test/java/com/stratio/tests/utils/ElasticSearchUtilsIT.java
+++ src/test/java/com/stratio/tests/utils/ElasticSearchUtilsIT.java
@@ -1,12 +1,8 @@
 package com.stratio.tests.utils;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.fail;
-import static org.testng.AssertJUnit.assertFalse;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -18,155 +14,97 @@ import java.util.Map;
 
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class ElasticSearchUtilsIT {
     private final Logger logger = LoggerFactory
             .getLogger(ElasticSearchUtilsIT.class);
-    @Test
+
+    private ElasticSearchUtils es_utils;
+
+    @BeforeMethod
     public void setSettingsTest() {
-        ElasticSearchUtils es_utils = new ElasticSearchUtils();
+        es_utils = new ElasticSearchUtils();
         LinkedHashMap<String,Object> settings_map = new LinkedHashMap<String,Object>();
         settings_map.put("cluster.name",System.getProperty("ES_CLUSTER", "elasticsearch"));
         es_utils.setSettings(settings_map);
-        assertThat("Non empty Exception list on boot",es_utils.getSettings().get("cluster.name"), equalTo(System
-                .getProperty("ES_CLUSTER", "elasticsearch")));
-        es_utils = null;
+        assertThat(es_utils.getSettings().get("cluster.name")).as("Non empty Exception list on boot").isEqualTo(System
+                .getProperty("ES_CLUSTER", "elasticsearch"));
     }
 
-    @Test
-    public void connectTest(){
-        ElasticSearchUtils es_utils = new ElasticSearchUtils();
+    @Test(expectedExceptions = java.net.UnknownHostException.class)
+    public void connectErrorTest() throws UnknownHostException {
         LinkedHashMap<String,Object> settings_map = new LinkedHashMap<String,Object>();
-        settings_map.put("cluster.name",System.getProperty("ES_CLUSTER", "elasticsearch"));
+        es_utils.setHost("www.badhost.ccom");
         es_utils.setSettings(settings_map);
-        try {
-            es_utils.connect();
-            es_utils.getClient().close();
-            es_utils = null;
-            assertTrue(true);
-        } catch (UnknownHostException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-    }
-
-    @Test
-    public void createIndexTest(){
-        ElasticSearchUtils es_utils = new ElasticSearchUtils();
-        LinkedHashMap<String,Object> settings_map = new LinkedHashMap<String,Object>();
-        settings_map.put("cluster.name",System.getProperty("ES_CLUSTER", "elasticsearch"));
-        es_utils.setSettings(settings_map);
-        try {
-            es_utils.connect();
-        }
-        catch (UnknownHostException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-        if(es_utils.indexExists("testindex")){
-            es_utils.dropSingleIndex("testindex");
-        }
-        boolean res = false;
-        try {
-            res = es_utils.createSingleIndex("testindex");
-        }catch(ElasticsearchException e){
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-        assertTrue(es_utils.indexExists("testindex"));
-    }
-
-    @Test
-    public void dropIndexTest(){
-        ElasticSearchUtils es_utils = new ElasticSearchUtils();
-        LinkedHashMap<String,Object> settings_map = new LinkedHashMap<String,Object>();
-        settings_map.put("cluster.name",System.getProperty("ES_CLUSTER", "elasticsearch"));
-        es_utils.setSettings(settings_map);
-        try {
-            es_utils.connect();
-        }
-        catch (UnknownHostException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-        if(!es_utils.indexExists("testindex")){
-            es_utils.createSingleIndex("testindex");
-        }
-         try {
-            es_utils.dropSingleIndex("testindex");
-        }catch(ElasticsearchException e){
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-        assertFalse(es_utils.indexExists("testindex"));
+        es_utils.connect();
         es_utils.getClient().close();
     }
 
     @Test
-    public void createMappingTest(){
-        ElasticSearchUtils es_utils = new ElasticSearchUtils();
-        LinkedHashMap<String,Object> settings_map = new LinkedHashMap<String,Object>();
-        settings_map.put("cluster.name",System.getProperty("ES_CLUSTER", "elasticsearch"));
-        es_utils.setSettings(settings_map);
-        try {
-            es_utils.connect();
+    public void connectTest() throws UnknownHostException {
+        es_utils.connect();
+        assertThat(es_utils.getClient().admin().cluster().prepareNodesInfo().all().execute()).isNotNull();
+        es_utils.getClient().close();
+    }
+
+    @Test
+    public void createIndexTest() throws UnknownHostException{
+        es_utils.connect();
+        if(es_utils.indexExists("testindex")){
+            es_utils.dropSingleIndex("testindex");
         }
-        catch (UnknownHostException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
+        es_utils.createSingleIndex("testindex");
+        assertThat(es_utils.indexExists("testindex")).isTrue();
+    }
+
+    @Test
+    public void dropIndexTest() throws UnknownHostException{
+        es_utils.connect();
+        if(!es_utils.indexExists("testindex")){
+            es_utils.createSingleIndex("testindex");
         }
+        es_utils.dropSingleIndex("testindex");
+        assertThat(es_utils.indexExists("testindex")).isFalse();
+        es_utils.getClient().close();
+    }
+
+    @Test
+    public void createMappingTest() throws UnknownHostException,IOException,InterruptedException{
+        es_utils.connect();
         if(es_utils.indexExists("testindex")){
             es_utils.dropSingleIndex("testindex");
         }
         ArrayList<XContentBuilder> mappingsource = new ArrayList<XContentBuilder>();
         XContentBuilder builder= null;
-        try {
-            builder = jsonBuilder()
+
+        builder = jsonBuilder()
                     .startObject()
                     .field("ident", 1)
                     .field("name", "test")
                     .field("money", 10.2)
                     .field("new", false).endObject();
 
-        } catch (IOException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(), false);
-        }
-            mappingsource.add(builder);
-
-        try {
-            es_utils.createMapping("testindex", "testmapping", mappingsource);
-           // Thread.sleep(2000);
-        }catch(ElasticsearchException e){
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Assert.assertTrue(es_utils.existsMapping("testindex","testmapping"));
+        mappingsource.add(builder);
+        es_utils.createMapping("testindex", "testmapping", mappingsource);
+        Thread.sleep(2000);
+        assertThat(es_utils.existsMapping("testindex","testmapping")).isTrue();
         es_utils.dropMapping("testindex","testmapping");
-        Assert.assertFalse(es_utils.existsMapping("testindex","testmapping"));
+        assertThat(es_utils.existsMapping("testindex","testmapping")).isFalse();
         es_utils.getClient().close();
     }
 
-
     @Test
-    public void searchFilterEquals(){
-        ElasticSearchUtils es_utils = new ElasticSearchUtils();
-        LinkedHashMap<String,Object> settings_map = new LinkedHashMap<String,Object>();
-        settings_map.put("cluster.name",System.getProperty("ES_CLUSTER", "elasticsearch"));
-        es_utils.setSettings(settings_map);
-        try {
-            es_utils.connect();
-        }
-        catch (UnknownHostException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
+    public void searchFilterEquals() throws UnknownHostException,IOException,InterruptedException{
+        es_utils.connect();
         if(es_utils.indexExists("testindex")){
             es_utils.dropSingleIndex("testindex");
         }
         ArrayList<XContentBuilder> mappingsource = new ArrayList<XContentBuilder>();
         XContentBuilder builder= null,builder2 = null;
-        try {
+
             builder = jsonBuilder()
                     .startObject()
                     .field("ident", 1)
@@ -180,36 +118,23 @@ public class ElasticSearchUtilsIT {
                     .field("money", 10.2)
                     .field("new", false).endObject();
 
-        } catch (IOException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(), false);
-        }
+
         mappingsource.add(builder);
         mappingsource.add(builder2);
-        try {
-            es_utils.createMapping("testindex", "testmapping", mappingsource);
-            // Thread.sleep(2000);
-        }catch(ElasticsearchException e){
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-        }
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            Assert.assertTrue("An exception has been thrown: " + e.toString(), false);
-        }
-        Assert.assertTrue(es_utils.existsMapping("testindex","testmapping"));
+        es_utils.createMapping("testindex", "testmapping", mappingsource);
+        Thread.sleep(2000);
+        assertThat(es_utils.existsMapping("testindex","testmapping")).isTrue();
         int res = 0;
         try {
             res = es_utils.searchSimpleFilterElasticsearchQuery("testindex", "testmapping", "ident", "1", "equals")
                     .size();
-        }catch(Exception e){
-            Assert.assertTrue("An exception has been thrown: " + e.toString(),false);
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        Assert.assertEquals(res, 1);
+        assertThat(res).isEqualTo(1);
         es_utils.dropMapping("testindex","testmapping");
         es_utils.dropAllIndexes();
-        Assert.assertFalse(es_utils.existsMapping("testindex","testmapping"));
+        assertThat(es_utils.existsMapping("testindex","testmapping")).isFalse();
         es_utils.getClient().close();
     }
-
 }
