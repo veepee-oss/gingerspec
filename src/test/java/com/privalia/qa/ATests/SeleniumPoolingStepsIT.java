c6111d6b210e74021387eaa01a277fa773396eab
Jose Fernandez <josefd8@gmail.com>
2017-11-20 14:51:41 +0100

    Created runner class for selenium feature file

---
+++ src/test/java/com/privalia/qa/ATests/SeleniumPoolingStepsIT.java
@@ -0,0 +1,23 @@
+package com.privalia.qa.ATests;
+
+import com.privalia.qa.cucumber.testng.CucumberRunner;
+import com.privalia.qa.data.BrowsersDataProvider;
+import com.privalia.qa.utils.BaseTest;
+import cucumber.api.CucumberOptions;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+@CucumberOptions(features = {"src/test/resources/features/seleniumPoolingSteps.feature"})
+public class SeleniumPoolingStepsIT extends BaseTest {
+
+    @Factory( dataProviderClass = BrowsersDataProvider.class, dataProvider = "availableUniqueBrowsers")
+    public SeleniumPoolingStepsIT(String browser) {
+        this.browser = browser;
+    }
+
+    @Test(enabled = true)
+    public void SeleniumPoolingStepsTest() throws Exception {
+        new CucumberRunner(this.getClass()).runCukes();
+    }
+
+}
