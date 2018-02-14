36c7b225385cc99c8915094acd2b09da88b187f0
oleksandr.tarasyuk <oleksandr.tarasyuk@ext.privalia.com>
2018-02-14 15:57:55 +0100

    jira change reference

--- src/main/java/com/privalia/qa/cucumber/testng/CucumberReporter.java
+++ src/main/java/com/privalia/qa/cucumber/testng/CucumberReporter.java
@@ -555,7 +555,7 @@ public class CucumberReporter implements Formatter, Reporter {
             } else if (ignored && (!ignoreReason || (ignoreReason && isJiraTicketDone) || (ignoreReason && isWrongTicket))) {
                 element.setAttribute(STATUS, "FAIL");
                 if (isJiraTicketDone) {
-                    msg1 = "The scenario was ignored due an already done (or in progress) ticket. " + "https://stratio.atlassian.net/browse/" + ticket;
+                    msg1 = "The scenario was ignored due an already done (or in progress) ticket. " + "http://jira.privalia.pin/browse/" + ticket;
                 } else if (isWrongTicket) {
                     msg1 = "The scenario was ignored due to unexistant ticket " + ticket;
                 } else {
