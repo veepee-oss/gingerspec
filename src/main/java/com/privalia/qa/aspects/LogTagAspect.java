951f0bec0c6fc751fa3c1b85853dc40c0efee6c6
Jose Fernandez <jose.fernandez@ext.privalia.com>
2018-07-02 09:50:26 +0200

    Added class for logging aspects

---
+++ src/main/java/com/privalia/qa/aspects/LogTagAspect.java
@@ -0,0 +1,9 @@
+package com.privalia.qa.aspects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogTagAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getCanonicalName());
+}
