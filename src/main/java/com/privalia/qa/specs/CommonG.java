623cd5cd7cd7c95befc9f8693d35ad4a0169c920
Jose Fernandez <josefd8@gmail.com>
2020-05-23 00:46:14 +0200

    Added changed in logger to allow the execution of feature files directly from intellij IDEA

--- src/main/java/com/privalia/qa/specs/CommonG.java
+++ src/main/java/com/privalia/qa/specs/CommonG.java
@@ -65,7 +65,7 @@ public class CommonG {
 
     private static final int DEFAULT_SLEEP_TIME = 1500;
 
-    private final Logger logger = LoggerFactory.getLogger(ThreadProperty.get("class"));
+    private final Logger logger = LoggerFactory.getLogger((ThreadProperty.get("class") != null ? ThreadProperty.get("class") : this.getClass().getName()));
 
     private WebDriver driver = null;
 
