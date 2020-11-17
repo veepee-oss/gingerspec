6f5ec7adee5d045de1d8dbfec7f59de171f1aa94
Jose Fernandez <josefd8@pop-os.localdomain>
2020-11-17 23:23:47 +0100

    Improved javadoc for kafka spec

--- src/main/java/com/privalia/qa/specs/KafkaGSpec.java
+++ src/main/java/com/privalia/qa/specs/KafkaGSpec.java
@@ -37,7 +37,16 @@ public class KafkaGSpec extends BaseGSpec {
 
     /**
      * Connect to Kafka.
+     * <p>
+     * Establish the connection to the Kafka cluster via the IP of the Zookeeper service. This is an initialization step necessary for all future steps
      *
+     * <pre>
+     * Example: Assuming zookeeper service is running at localhost:2181
+     * {@code
+     *      Given I connect to kafka at 'localhost:2181'
+     * }
+     * </pre>
+     * @see #iCloseTheConnectionToKafka()
      * @param zkHost ZK host
      * @param zkPath ZK port
      * @throws UnknownHostException exception
@@ -52,7 +61,22 @@ public class KafkaGSpec extends BaseGSpec {
 
     /**
      * Create a Kafka topic.
+     * <p>
+     * Creates a kafka topic with the given name. It can also create the topic only if it doesn't exists. All topics are created by default
+     * with 1 partition and a replication factor of 1
      *
+     * <pre>
+     * Example: Create the topic 'testqa'
+     * {@code
+     *      Given I create a Kafka topic named 'testqa'
+     * }
+     * Example: Create the topic 'testqa' if it doesn't exists
+     * {@code
+     *      Given I create a Kafka topic named 'testqa' if it doesn't exists
+     * }
+     * </pre>
+     *
+     * @see #deleteKafkaTopic(String)
      * @param topic_name topic name
      * @param ifExists   String fro matching optional text in Gherkin
      * @throws Exception Exception
@@ -73,7 +97,14 @@ public class KafkaGSpec extends BaseGSpec {
 
     /**
      * Delete a Kafka topic.
+     * <pre>
+     * Example: Delete the topic 'testqa'
+     * {@code
+     *      When I delete a Kafka topic named 'testqa'
+     * }
+     * </pre>
      *
+     * @see #createKafkaTopic(String, String)
      * @param topic_name topic name
      * @throws Exception Exception
      */
@@ -83,8 +114,17 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
+     * Increase partitions in kafka topic
+     * <p>
      * Modify partitions in a Kafka topic by increasing the current number of partitions in the topic by the specified
      * number. Mind that the number of partitions for a topic can only be increased once its created
+     *
+     * <pre>
+     * Example: Increase partitions topic 'testqa'
+     * {@code
+     *      Given I increase '1' partitions in a Kafka topic named 'testqa'
+     * }
+     * </pre>
      *
      * @param numPartitions number of partitions to add to the current amount of partitions for the topic
      * @param topic_name    topic name
@@ -98,11 +138,30 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
-     * Sends a message to a Kafka topic. By default, this steps uses StringSerializer and StringDeserializer for
+     * Sends a message to a Kafka topic.
+     * <p>
+     * By default, this steps uses StringSerializer and StringDeserializer for
      * the key/value of the message, and default properties for the producer. This steps can also verify if a message
      * with the corresponding key and value already exists in the topic before inserting. Remember that for a consumer to
-     * be able to read all the messages from a topic, it must use a new group.id (not used before in that topic)
+     * be able to read all the messages from a topic, it must use a new group.id (not used before in that topic). When reading
+     * from a kafka topic with a consumer, kafka will return the next message by offset not read for that group.
      *
+     * <pre>
+     * Example: For sending a simple message (only specifying value)
+     * {@code
+     *      Given I send a message 'hello' to the kafka topic named 'testqa'
+     * }
+     * Example: For sending a message with key and value (specifying kay and value)
+     * {@code
+     *      Given I send a message 'hello' to the kafka topic named 'testqa' with key 'keyvalue'
+     * }
+     * Example: To insert a message only if the exact key-value combination does not already exists in the topic
+     * {@code
+     *      Given I send a message 'hello' to the kafka topic named 'testqa' with key 'keyvalue' if not exists
+     * }
+     * </pre>
+     *
+     * @see #sendAMessageWithDatatable(String, String, String, DataTable)
      * @param message    string that you send to topic
      * @param topic_name topic name
      * @param recordKey  the record key
@@ -126,8 +185,36 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
-     * Sends a message to a Kafka topic. This steps allows to modify any property of the producer before sending
+     * Sends a message to a Kafka topic with properties.
+     * <p>
+     * Similar to the {@link #sendAMessage(String, String, String, String)} step, but gives the possibility to override
+     * the properties of the producer before sending a message. For example, the properties of the producer can be altered
+     * to change the default serializer for key/value before sending. In this case, for the key.serializer property the
+     * value should be "org.apache.kafka.common.serialization.StringSerializer" and for the value.serializer the value
+     * should be "org.apache.kafka.common.serialization.LongSerializer".
+     * <p>
+     * The library will try to automatically cast the message to the type of the specified value.serializer property. So,
+     * for example, trying to send the message "hello" using LongSerializer for value.serializer will produce an error.
      *
+     * <pre>
+     * Example: For sending a message to topic 'longTopic' with key as String and value as Long
+     * {@code
+     *      When I send a message '1234567890' to the kafka topic named 'longTopic' with:
+     *          | key.serializer    | org.apache.kafka.common.serialization.StringSerializer |
+     *          | value.serializer  | org.apache.kafka.common.serialization.LongSerializer   |
+     * }
+     *
+     * Other common properties for a Kafka consumer:
+     * - bootstrap.servers (defaults to 0.0.0.0:9092)
+     * - acks (defaults to "all")
+     * - retries (defaults to 0)
+     * - batch.size (defaults to 16384)
+     * - linger.ms (defaults to 1)
+     * - buffer.memory (defaults to 33554432)
+     * - client.id (defaults to KafkaQAProducer)
+     * </pre>
+     *
+     * @see #sendAMessage(String, String, String, String)
      * @param message    Message to send (will be converted to the proper type specified by the value.serializer prop). String is default
      * @param topic_name Name of the topic where to send the message
      * @param recordKey  Key of the kafka record
@@ -151,7 +238,15 @@ public class KafkaGSpec extends BaseGSpec {
 
     /**
      * Check that a kafka topic does not exist
+     * <pre>
+     * Example:
+     * {@code
+     *      Then A kafka topic named 'testqa' does not exist
+     * }
+     * </pre>
      *
+     * @see #createKafkaTopic(String, String)
+     * @see #deleteKafkaTopic(String)
      * @param topic_name name of topic
      * @throws KeeperException      KeeperException
      * @throws InterruptedException InterruptedException
@@ -175,10 +270,29 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
+     * Check if message exists
+     * <p>
      * Pools the given topic for messages and checks if any have the given value. By default, this method
      * uses String Serializer/Deserializer to read the messages from the topic (as well as all the default properties for
-     * the consumer)
+     * the consumer).
+     * <p>
+     * Unless specified, the method will only look for records that contain the specific message in the value of the kafka
+     * record but it can also be used.
      *
+     * <pre>
+     * Example: Check if the topic contains the message with the given value
+     * {@code
+     *      Then The kafka topic 'testqa' has a message containing 'hello'
+     * }
+     * Example: Check if the topic contains the message with the given key
+     * {@code
+     *      Then The kafka topic 'testqa' has a message containing 'hello' as key
+     * }
+     * </pre>
+     *
+     * @see #theKafkaTopicStringTopicHasAMessageContainingHelloWith(String, String, String, DataTable)
+     * @see #theKafkaTopicAvroTopicHasAnAvroMessageRecordWith(String, String, DataTable)
+     * @see #theKafkaTopicHasAnAvroMessageWith(String, String, int, DataTable)
      * @param topic   Topic to poll
      * @param content Value to look for (as String)
      * @param key     key of the record
@@ -196,6 +310,16 @@ public class KafkaGSpec extends BaseGSpec {
     /**
      * Check that a kafka topic exist
      *
+     * <pre>
+     * Example: Verify the topic 'testqa' exists
+     * {@code
+     *      Then A kafka topic named 'testqa' exists
+     * }
+     * </pre>
+     *
+     * @see #createKafkaTopic(String, String)
+     * @see #deleteKafkaTopic(String)
+     * @see #kafkaTopicNotExist(String)
      * @param topic_name name of topic
      * @throws KeeperException      KeeperException
      * @throws InterruptedException InterruptedException
@@ -208,9 +332,19 @@ public class KafkaGSpec extends BaseGSpec {
 
 
     /**
+     * Sets URL of schema registry.
+     * <p>
      * Initializes the remote URL of the schema registry service for all future requests. Also sets the property
      * schema.registry.url in the consumer and producer properties
      *
+     * <pre>
+     * Example: To set the schema registry at localhost:8081:
+     * {@code
+     *      Given My schema registry is running at 'localhost:8081'
+     * }
+     * </pre>
+     *
+     * @see #iRegisterANewVersionOfASchemaUnderTheSubject(String, String)
      * @param host Remote host and port (defaults to http://0.0.0.0:8081)
      * @throws Throwable Throwable
      */
@@ -221,7 +355,29 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
+     * Adds a new schema to the schema register.
+     * <p>
      * Generates a POST to the schema register to add a new schema for the given subject
+     * <pre>
+     * Example: Assuming the file located under schemas/recordSchema.avsc contains the following valid schema
+     *
+     * {
+     *     "namespace": "com.mynamespace",
+     *     "type": "record",
+     *     "name": "Record",
+     *     "fields": [
+     *         { "name": "str1", "type": "string" },
+     *         { "name": "str2", "type": "string" },
+     *         { "name": "int1", "type": "int" }
+     *      ]
+     * }
+     *
+     * Then, to set it at the schema registry at localhost:8081:
+     * {@code
+     *      Given My schema registry is running at 'localhost:8081'
+     *      Then I register a new version of a schema under the subject 'record' with 'schemas/recordSchema.avsc'
+     * }
+     * </pre>
      *
      * @param subjectName Name of the subject where register the new schema
      * @param filepath    Path of the file containing the schema
@@ -237,9 +393,40 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
+     * Reads message from topic with properties
+     * <p>
      * Reads messages from the beginning of the topic with the specified properties for the consumer. The message is casted to the
      * correct type based on the given value.deserializer property (uses String deserializer by default)
+     * <p>
+     * For example, the properties of the consumer can be altered to change the default deserializer for key/value when reading.
+     * In this case, for the key.deserializer property the value should be "org.apache.kafka.common.serialization.StringDeserializer"
+     * and for the value.deserializer the value should be "org.apache.kafka.common.serialization.LongDeserializer".
+     * <p>
+     * The library will try to automatically cast the message to the type of the specified value.deserializer property.
+     * So, for example, trying to read the message "hello" using LongSerializer for value.serializer will produce an error.
      *
+     * <pre>
+     * Example: Setting the read properties as String for key and Long for value before reading
+     * {@code
+     *      Then The kafka topic 'longTopic' has a message containing '1234567890' with:
+     *          | key.deserializer    | org.apache.kafka.common.serialization.StringDeserializer |
+     *          | value.deserializer  | org.apache.kafka.common.serialization.LongDeserializer   |
+     * }
+     *
+     * Other common properties for a Kafka consumer:
+     *
+     * - bootstrap.servers (defaults to 0.0.0.0:9092)
+     * - group.id (defaults to "test")
+     * - enable.auto.commit (defaults to true)
+     * - auto.offset.reset (defaults to 'earliest')
+     * - auto.commit.intervals.ms (defaults to 1000)
+     * - session.timeout (defaults to 10000)
+     *
+     * </pre>
+     *
+     * @see #checkMessages(String, String, String)
+     * @see #theKafkaTopicHasAnAvroMessageWith(String, String, int, DataTable)
+     * @see #theKafkaTopicAvroTopicHasAnAvroMessageRecordWith(String, String, DataTable)
      * @param topicName Name of the topic where to send the message
      * @param message   Message to send (will be converted to the correct type according to the value.deserializer property)
      * @param isKey     the is key
@@ -288,12 +475,31 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
-     * Creates an Avro record from the specified schema. The record is created as a {@link GenericRecord}
+     * Creates an Avro record from the specified schema.
+     * <p>
+     * The record is created as a {@link GenericRecord} and the user can dynamically specify the properties values.
      *
+     * <pre>
+     * Example: To create a new GenericRecord with name 'record' using the schema under schemas/recordSchema.avsc
+     * {@code
+     *      Given My schema registry is running at 'localhost:8081'
+     *      Then I register a new version of a schema under the subject 'record' with 'schemas/recordSchema.avsc'
+     *      Then I create the avro record 'record' from the schema in 'schemas/recordSchema.avsc' with:
+     *          | str1    | str1 |
+     *          | str2    | str2 |
+     *          | int1    |   1  |
+     * }
+     *
+     * The library will automatically cast the variables to the correct types specified in the *.avsc file. So, trying
+     * to insert an string for "int1" will generate an error
+     * </pre>
+     *
+     * @see #iSendTheAvroRecordRecordToTheKafkaTopic(String, String, String, DataTable)
+     * @see #iCreateTheAvroRecordRecordUsingVersionOfSubjectRecordFromRegistryWith(String, String, String, String, DataTable)
      * @param recordName Name of the Avro generic record
      * @param schemaFile File containing the schema of the message
      * @param seedFile   the seed file
-     * @param table      Table containen the values for the fields on the schema. (Values will be converted according to field type)
+     * @param table      Table contains the values for the fields on the schema. (Values will be converted according to field type)
      * @throws Throwable Throwable
      */
     @Then("^I create the avro record '(.+?)' from the schema in '(.+?)'( based on '(.+?)')? with:$")
@@ -360,9 +566,19 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
-     * Send the previously created Avro record to the given topic. The value.serializer property for the producer is set to KafkaAvroSerializer
-     * automatically
+     * Send the previously created Avro record.
+     * <p>
+     * The value.serializer property for the producer is set to KafkaAvroSerializer automatically
      *
+     * <pre>
+     * Example:
+     * {@code
+     *      When I send the avro record 'record' to the kafka topic 'avroTopic' with:
+     *          | key.serializer    | org.apache.kafka.common.serialization.StringSerializer |
+     * }
+     * </pre>
+     *
+     * @see #iCreateTheAvroRecordRecord(String, String, String, DataTable)
      * @param genericRecord Name of the record to send
      * @param topicName     Topic where to send the record
      * @param recordKey     Record key
@@ -387,9 +603,21 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
+     * Search the topic for the given Avro record.
+     * <p>
      * Reads the specified topic from beginning for the specified avro record. The consumer value.deserializer property is automatically
      * set to KafkaAvroDeserializer
      *
+     * <pre>
+     * Example:
+     * {@code
+     *      Then The kafka topic 'avroTopic' has an avro message 'record' with:
+     *          | key.deserializer    | org.apache.kafka.common.serialization.StringDeserializer |
+     * }
+     * </pre>
+     *
+     * @see #iCreateTheAvroRecordRecord(String, String, String, DataTable)
+     * @see #iSendTheAvroRecordRecordToTheKafkaTopic(String, String, String, DataTable)
      * @param topicName  Topic to read from
      * @param avroRecord Name of the record to read
      * @param dataTable  Table containing modifications for the consumer properties
@@ -417,8 +645,21 @@ public class KafkaGSpec extends BaseGSpec {
     }
 
     /**
+     * Modify consumer properties
+     * <p>
      * A single step for modifying the consumer properties for the rest of the scenario.
      *
+     * <pre>
+     * Example: To change consumer properties
+     * {@code
+     *      Then I configure the kafka consumers with:
+     *          | group.id           | !{ID}                                                                                               |
+     *          | key.deserializer   | org.apache.kafka.common.serialization.StringDeserializer                                            |
+     *          | value.deserializer | org.apache.kafka.common.serialization.StringDeserializer                                            |
+     *          | bootstrap.servers  | srdc1kafkassl5.privalia.pin:9092,srdc1kafkassl9.privalia.pin:9092,srdc1kafkassl10.privalia.pin:9092 |
+     * }
+     * </pre>
+     * @see #iConfigureProducerProperties(DataTable)
      * @param dataTable table with consumer properties
      */
     @Then("^I configure the kafka consumers with:$")
@@ -435,8 +676,22 @@ public class KafkaGSpec extends BaseGSpec {
 
 
     /**
+     * Modify producer properties
+     * <p>
      * A single step for modifying the producer properties for the rest of the scenario.
      *
+     * <pre>
+     * Example: To change producer properties settings:
+     * {@code
+     *      Then I configure the kafka producer with:
+     *          | client.id         | QAderInjector                                                                                       |
+     *          | key.serializer    | org.apache.kafka.common.serialization.StringSerializer                                              |
+     *          | value.serializer  | org.apache.kafka.common.serialization.StringSerializer                                              |
+     *          | bootstrap.servers | srdc1kafkassl5.privalia.pin:9092,srdc1kafkassl9.privalia.pin:9092,srdc1kafkassl10.privalia.pin:9092 |
+     * }
+     * </pre>
+     *
+     * @see #iConfigureConsumerProperties(DataTable)
      * @param dataTable table with consumer properties
      */
     @Then("^I configure the kafka producer with:$")
@@ -468,6 +723,14 @@ public class KafkaGSpec extends BaseGSpec {
 
     /**
      * Performs a partial property matching on the avro records returned
+     *
+     * <pre>
+     * Example:
+     * {@code
+     *      Then The kafka topic 'avroTopic' has at least '1' an avro message with:
+     *          | user.id    | Paul |
+     * }
+     * </pre>
      *
      * @param topicName     Name of the topic to read messages from
      * @param atLeast       Indicates to find at least the expectedCount. If ignored, asserts the exact quantity is found
