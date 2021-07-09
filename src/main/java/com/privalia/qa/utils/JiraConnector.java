3c7d0ef051aef4aa680feec50f57fa8ec0746910
jose.fernandez <josefd8@gmaill.com>
2021-07-09 14:45:40 +0200

    Added basic javadoc

--- src/main/java/com/privalia/qa/utils/JiraConnector.java
+++ src/main/java/com/privalia/qa/utils/JiraConnector.java
@@ -9,6 +9,10 @@ import org.apache.commons.text.StringSubstitutor;
 import net.minidev.json.JSONArray;
 import java.util.concurrent.Future;
 
+/**
+ * An small utility for interacting with entities in Jira
+ * @author José Fernández
+ */
 public class JiraConnector {
 
     public static final String JIRA_PROPERTIES_FILE = "jira.properties";
@@ -17,6 +21,13 @@ public class JiraConnector {
 
     AsyncHttpClient client = new AsyncHttpClient();
 
+    /**
+     * Reads the given key from the properties file. The system will try to locate the key first in
+     * the maven variables (System.getProperty) and if not found will look for it in the properties file.
+     * If the value is still not found it will return the default value (if provided) or an exception
+     * @param property  key
+     * @return          value
+     */
     public String getProperty(String property) {
 
         interpolator.setEnableUndefinedVariableException(true);
@@ -28,6 +39,12 @@ public class JiraConnector {
         return interpolator.replace("${properties:src/test/resources/" + JIRA_PROPERTIES_FILE + "::" + property + "}");
     }
 
+    /**
+     * Retrieves the current entity status
+     * @param entity        Entity identifier (i.e QMS-123)
+     * @return              Status as string (i.e 'In Progress')
+     * @throws Exception    Exception
+     */
     public String getEntityStatus(String entity) throws Exception {
 
         String jiraURL = this.getProperty("jira.server.url");
@@ -49,7 +66,12 @@ public class JiraConnector {
         return JsonPath.read(r.getResponseBody(),"$.fields.status.name").toString().toUpperCase();
     }
 
-
+    /**
+     * Determines if the entity status matches any of the expected statuses
+     * @param entity        Entity identifier (i.e QMS-123)
+     * @return              True if the entity status is within the expected statuses
+     * @throws Exception    Exception
+     */
     public Boolean entityShouldRun(String entity) throws Exception {
 
         String[] valid_statuses = this.getProperty("jira.valid.runnable.statuses:-Done,Deployed").split(",");
@@ -64,6 +86,13 @@ public class JiraConnector {
         return false;
     }
 
+    /**
+     * Change the status of an entity to the Given Status by name. The status name should match exactly a valid
+     * status for that entity
+     * @param entity        Entity identifier (i.e QMS-123)
+     * @param new_status    New status (i.e 'Done')
+     * @throws Exception    Exception
+     */
     public void transitionEntityToGivenStatus(String entity, String new_status) throws Exception {
 
         int targetTransition = this.getTransitionIDForEntityByName(entity, new_status);
@@ -88,6 +117,13 @@ public class JiraConnector {
 
     }
 
+    /**
+     * Gets the id of the transition by the given name
+     * @param entity            Entity identifier (i.e QMS-123)
+     * @param transitionName    Transition name (i.e 'In Progress')
+     * @return                  Id of the transition for that name
+     * @throws Exception        Exception
+     */
     public int getTransitionIDForEntityByName(String entity, String transitionName) throws Exception {
 
         String jiraURL = this.getProperty("jira.server.url");
@@ -117,6 +153,12 @@ public class JiraConnector {
 
     }
 
+    /**
+     * Adds a new comment to the entity
+     * @param entity        Entity identifier (i.e QMS-123)
+     * @param message       Message to post
+     * @throws Exception    Exception
+     */
     public void postCommentToEntity(String entity, String message) throws Exception {
 
         String jiraURL = this.getProperty("jira.server.url");
@@ -138,6 +180,12 @@ public class JiraConnector {
         }
     }
 
+    /**
+     * Transition (change status) of the entity to the value provided in the properties file. Will
+     * default to "In Progress" is the value is not found
+     * @param entity        Entity identifier (i.e QMS-123)
+     * @throws Exception    Exception
+     */
     public void transitionEntity(String entity) throws Exception {
 
         String jiraTransitionToStatus = this.getProperty("jira.transition.if.fail.status:-In Progress");
