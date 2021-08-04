0af9052b0e4ba2034f57422c96389091df036b20
jose.fernandez <josefd8@gmaill.com>
2021-08-04 22:57:51 +0200

    One of the pointcut of the ifStatement aspect was rewritten to fix a bug where the skipped text were not shown as skipped in the reports

--- src/main/java/com/privalia/qa/aspects/ifStatementAspect.java
+++ src/main/java/com/privalia/qa/aspects/ifStatementAspect.java
@@ -21,16 +21,19 @@ import com.privalia.qa.lookups.EnvPropertyLookup;
 import com.privalia.qa.lookups.LowerCaseLookup;
 import com.privalia.qa.lookups.UpperCaseLookUp;
 import io.cucumber.core.backend.TestCaseState;
+import io.cucumber.plugin.event.Result;
 import io.cucumber.plugin.event.Status;
+import io.cucumber.plugin.event.TestCase;
+import io.cucumber.plugin.event.TestStep;
 import org.apache.commons.text.StringSubstitutor;
 import org.apache.commons.text.lookup.StringLookup;
 import org.apache.commons.text.lookup.StringLookupFactory;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 
 import java.lang.reflect.Field;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -39,8 +42,8 @@ import java.util.regex.Pattern;
 /**
  * Aspect to control the conditional execution of steps based on a given expression.
  *
- * @see com.privalia.qa.specs.UtilsGSpec#ifStamenetBeginBlock(String)
  * @author Jose Fernandez
+ * @see com.privalia.qa.specs.UtilsGSpec#ifStamenetBeginBlock(String)
  */
 @Aspect
 public class ifStatementAspect {
@@ -62,6 +65,7 @@ public class ifStatementAspect {
 
     /**
      * This pointcut is triggered right before the step is executed
+     *
      * @param state state
      */
     @Pointcut("execution (* io.cucumber.core.runner.PickleStepDefinitionMatch.runStep(..)) && args(state)")
@@ -96,37 +100,43 @@ public class ifStatementAspect {
     }
 
     /**
-     * This pointcut is triggered right before reading the result of the execution of a step. If the step was not executed
-     * the status is changed to "SKIPPED", so it is colored in a different way in the console and in reports
+     * This pointcut is triggered right after the creation of the TestStepFinished event. If the step was not executed
+     * the status of the result is changed to "SKIPPED", so it is colored in a different way in the console and in reports
+     *
+     * @param timeInstant timeInstant
+     * @param testCase    testCase
+     * @param testStep    testStep
+     * @param result      result
      */
-    @Pointcut("execution (* io.cucumber.plugin.event.TestStepFinished.getResult(..)) && args()")
-    protected void getResult() {
+    @Pointcut("execution (io.cucumber.plugin.event.TestStepFinished.new(..)) && args(timeInstant, testCase, testStep, result)")
+    protected void setResult(Instant timeInstant, TestCase testCase, TestStep testStep, Result result) {
     }
 
-    @Around(value = "getResult()")
-    public io.cucumber.plugin.event.Result aroundResult(ProceedingJoinPoint pjp) throws NoSuchFieldException, IllegalAccessException {
+    @After(value = "setResult(timeInstant, testCase, testStep, result)")
+    public void aroundSetResult(JoinPoint jp, Instant timeInstant, TestCase testCase, TestStep testStep, Result result) throws NoSuchFieldException, IllegalAccessException {
 
-        Object TestStepFinished = pjp.getThis();
-        Field result = TestStepFinished.getClass().getDeclaredField("result");
-        result.setAccessible(true);
-        io.cucumber.plugin.event.Result resultObject = (io.cucumber.plugin.event.Result) result.get(TestStepFinished);
+        Object testStepFinished = jp.getThis();
+        Field currentResult = testStepFinished.getClass().getDeclaredField("result");
+        currentResult.setAccessible(true);
+        io.cucumber.plugin.event.Result currentResultObject = (io.cucumber.plugin.event.Result) currentResult.get(testStepFinished);
+        io.cucumber.plugin.event.Result skippedResult = new io.cucumber.plugin.event.Result(Status.SKIPPED, currentResultObject.getDuration(), currentResultObject.getError());
 
         if (step_text.startsWith("if (") || step_text.matches("}")) {
-            return resultObject;
+            return;
         }
 
-        if (step_should_run) {
-            return resultObject;
-        } else {
-            return new io.cucumber.plugin.event.Result(Status.SKIPPED, resultObject.getDuration(), resultObject.getError());
+        if (!step_should_run) {
+            currentResult.set(testStepFinished, skippedResult);
         }
+
     }
 
     /**
      * Captures the given javascript statement and determines if it resolves
      * to true or false.
-     * @param text      Step text
-     * @return          True if the expression can be resolved to true, false otherwise
+     *
+     * @param text Step text
+     * @return True if the expression can be resolved to true, false otherwise
      */
     private Boolean validateStatement(String text) {
 
