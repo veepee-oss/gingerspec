d007730c3591e377e32672f6fda388dcc895bce7
Jose Fernandez Duque <jose.fernandez@VPMAC-FVFFQ1KSQ05P.local>
2022-06-15 15:36:56 +0200

    Removed replacementAspect feature from restAssured runner class

--- src/test/java/com/privalia/qa/ATests/RestAssuredStepsIT.java
+++ src/test/java/com/privalia/qa/ATests/RestAssuredStepsIT.java
@@ -22,7 +22,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {
         "com.privalia.qa.cucumber.reporter.gingerHtmlFormatter:target/test",
         },
-        features = {"src/test/resources/features/restAssured.feature", "src/test/resources/features/replacementAspect.feature"},
+        features = {"src/test/resources/features/restAssured.feature"},
         glue = "com.privalia.qa.specs")
 public class RestAssuredStepsIT extends BaseGTest {
 
